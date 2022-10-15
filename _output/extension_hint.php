e4ab8d8de30efef481f46bc67531523b3e313efd
Jaysc <jaysc@users.noreply.github.com>
2022-10-15 19:27:47 +0100

    'v1'

--- _output/extension_hint.php
+++ _output/extension_hint.php
@@ -3,7 +3,7 @@
 // ################## THIS IS A GENERATED FILE ##################
 // DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
 
-namespace Jaysc\Quoteby\XF\Entity
+namespace Jaysc\QuoteBy\XF\Entity
 {
 	class XFCP_Post extends \XF\Entity\Post {}
 }
